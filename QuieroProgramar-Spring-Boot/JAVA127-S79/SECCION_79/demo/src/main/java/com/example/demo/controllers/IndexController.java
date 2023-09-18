6200a7e357e6ea3741facf2332d6a15b34ba66ac
ARHL2023 <TuCorreoElectronico>
2023-09-18 09:51:53 -0500

    @ModelAttribute (606)

--- QuieroProgramar-Spring-Boot/JAVA127-S79/SECCION_79/demo/src/main/java/com/example/demo/controllers/IndexController.java
+++ QuieroProgramar-Spring-Boot/JAVA127-S79/SECCION_79/demo/src/main/java/com/example/demo/controllers/IndexController.java
@@ -4,6 +4,7 @@ import com.example.demo.models.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -28,26 +29,39 @@ public class IndexController {
         usuario.setApellido("Holguin");
         usuario.setEmail("andy@gmail.com");
 
-        model.addAttribute("perfil",usuario);//key y valor
+        model.addAttribute("perfil", usuario);//key y valor
         model.addAttribute("titulo", "Perfil de usuario : ".concat(usuario.getNombre()));
 
         return "perfil";
     }
 
-
     @GetMapping("/listar")
-    public String listar(Model model){
+    public String listar(Model model) {
 
-        List<Usuario> listaUsuarios = new ArrayList<>();
-
-        listaUsuarios.add(new Usuario("Andy","Holguin","andy@gmail.com"));
-        listaUsuarios.add(new Usuario("Lady","mite","lady@gmail.com"));
-        listaUsuarios.add(new Usuario("Nuevo","Nuevo2"));
+//        List<Usuario> listaUsuarios = new ArrayList<>();
+//
+//        listaUsuarios.add(new Usuario("Andy", "Holguin", "andy@gmail.com"));
+//        listaUsuarios.add(new Usuario("Lady", "mite", "lady@gmail.com"));
+//        listaUsuarios.add(new Usuario("Nuevo", "Nuevo2"));
 
         model.addAttribute("titulo", "Lista de usuarios");
-        model.addAttribute("usuarios",listaUsuarios);
+//        model.addAttribute("usuarios", listaUsuarios);
 
         return "listar";
+    }
+
+
+    //@ModelAttribute --se aplica a todos los metodos del controlador, aqui sera con index
+    //es el vlor con el que se guarda en la vista como si fuesee model.addAttribute("usuarios", listaUsuarios);
+    @ModelAttribute("usuarios")
+    public List<Usuario> poblarUsuarios() {
+        List<Usuario> listaUsuarios = new ArrayList<>();
+
+        listaUsuarios.add(new Usuario("Andy", "Holguin", "andy@gmail.com"));
+        listaUsuarios.add(new Usuario("Lady", "mite", "lady@gmail.com"));
+        listaUsuarios.add(new Usuario("Nuevo", "Nuevo2"));
+
+        return listaUsuarios;//retorna la lista
     }
 
 }
