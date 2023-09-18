900dff236bbfee81bdc967f14316f41c96af64f3
ARHL2023 <TuCorreoElectronico>
2023-09-18 11:11:03 -0500

    @RequestParam y HttpServletRequest - obtener varios parametros de la URL (608)

--- QuieroProgramar-Spring-Boot/JAVA127-S79/SECCION_79/demo/src/main/java/com/example/demo/controllers/EjemploParamsController.java
+++ QuieroProgramar-Spring-Boot/JAVA127-S79/SECCION_79/demo/src/main/java/com/example/demo/controllers/EjemploParamsController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,16 +11,41 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/params")
 public class EjemploParamsController {
 
+    @GetMapping("/")
+    public String index() {
+        return "params/index";
+    }
+
     @GetMapping("/string")
-    public String param( @RequestParam(required = false) String texto, Model model){
+    public String param(@RequestParam(required = false) String texto, Model model) {
         // required es por si no pase un parametro pase un null y un error- tambieen podria usar defaultValue="un  valor cualquiera"
-        model.addAttribute("resultado","El texto enviado es "+texto);
+        model.addAttribute("resultado", "El texto enviado es " + texto);
         return "params/ver";
     }
 
-    @GetMapping("/")
-    public String index(){
-        return "params/index";
+    @GetMapping("/mix-params")
+    public String param(@RequestParam(required = false) String saludo, @RequestParam Integer numero, Model model) {
+        // required es por si no pase un parametro pase un null y un error- tambieen podria usar defaultValue="un  valor cualquiera"
+        model.addAttribute("resultado", "El saludo enviado es " + saludo + " Y el numero es " + numero);
+        return "params/ver";
     }
+
+
+    @GetMapping("/mix-params-request")
+    public String param(HttpServletRequest request, Model model) {
+
+        String saludo = request.getParameter("saludo");
+        Integer numero = null;
+
+        try {
+            numero = Integer.parseInt(request.getParameter("numero"));
+        } catch (NumberFormatException e) {
+            numero = 0;
+        }
+
+        model.addAttribute("resultado", "El saludo enviado es " + saludo + " Y el numero es " + numero);
+        return "params/ver";
+    }
+
 
 }
