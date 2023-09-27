3de1685f3cfcc6b460dbed7b03fff8e8705a234d
ARHL2023 <TuCorreoElectronico>
2023-09-27 09:47:54 -0500

    practica seccion 79

---
+++ QuieroProgramar-Spring-Boot/JAVA127-S79/SECCION_79/demoPracticeSECCION79/src/main/java/com/example/demoPracticeSECCION79/DemoPracticeSeccion79Application.java
@@ -0,0 +1,13 @@
+package com.example.demoPracticeSECCION79;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPracticeSeccion79Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPracticeSeccion79Application.class, args);
+	}
+
+}
