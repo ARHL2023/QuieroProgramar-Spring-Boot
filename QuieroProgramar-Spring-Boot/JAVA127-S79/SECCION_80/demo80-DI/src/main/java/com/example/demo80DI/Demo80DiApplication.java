443e1d765b3d32044f78072e385fc81cbf8ef5e8
ARHL2023 <TuCorreoElectronico>
2023-09-27 09:58:32 -0500

    init 80

---
+++ QuieroProgramar-Spring-Boot/JAVA127-S79/SECCION_80/demo80-DI/src/main/java/com/example/demo80DI/Demo80DiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo80DI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo80DiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo80DiApplication.class, args);
+	}
+
+}
---
+++ QuieroProgramar-Spring-Boot/JAVA127-S79/SECCION_80/demo80-DI/src/test/java/com/example/demo80DI/Demo80DiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo80DI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo80DiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
