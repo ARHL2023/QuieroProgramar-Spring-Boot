e5871b78d8840eaa814dc3e7feec3c6d14b71d83
ARHL2023 <TuCorreoElectronico>
2023-10-02 10:25:16 -0500

    inyeccion de dependencias practica Factura (seccion 80 completo)

---
+++ QuieroProgramar-Spring-Boot/JAVA127-S79/SECCION_80/demo80DIPRACTICEFACTURA/src/main/java/com/example/demo80DIPRACTICEFACTURA/Demo80DipracticefacturaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo80DIPRACTICEFACTURA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo80DipracticefacturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo80DipracticefacturaApplication.class, args);
+	}
+
+}
---
+++ QuieroProgramar-Spring-Boot/JAVA127-S79/SECCION_80/demo80DIPRACTICEFACTURA/src/test/java/com/example/demo80DIPRACTICEFACTURA/Demo80DipracticefacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo80DIPRACTICEFACTURA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo80DipracticefacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
