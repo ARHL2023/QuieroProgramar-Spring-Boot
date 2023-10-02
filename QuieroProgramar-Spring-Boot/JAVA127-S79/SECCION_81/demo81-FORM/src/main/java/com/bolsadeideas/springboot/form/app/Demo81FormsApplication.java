1df70228f45c6acbdbab46adf66b4c1c58bb311d
ARHL2023 <TuCorreoElectronico>
2023-10-02 14:54:33 -0500

    init seccion 81

---
+++ QuieroProgramar-Spring-Boot/JAVA127-S79/SECCION_81/demo81-FORM/src/main/java/com/bolsadeideas/springboot/form/app/Demo81FormsApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.form.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo81FormsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo81FormsApplication.class, args);
+    }
+
+}//
