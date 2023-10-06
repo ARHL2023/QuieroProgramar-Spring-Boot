e544a90121a74d29e4fd48fec9dc8e76f7135166
ARHL2023 <TuCorreoElectronico>
2023-10-05 19:32:16 -0500

    637-638 (ARCHIVO FORM 2 CONTINUAREMOS DESDE AQUI)Validación del formulario usando anotaciones, ADD ERRORES DE VISTA AL FORMULARIO

---
+++ QuieroProgramar-Spring-Boot/JAVA127-S79/SECCION_81/demo81-FORM2/src/main/java/com/bolsadeideas/springboot/form/app/Demo81Form2Application.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.form.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo81Form2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo81Form2Application.class, args);
+	}
+
+}
