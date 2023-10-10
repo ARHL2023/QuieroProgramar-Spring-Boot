54befb6d9bb5ac732fdd9b37abe7f175ca66e5f5
ARHL2023 <TuCorreoElectronico>
2023-10-10 09:53:12 -0500

    642. La anotación @Size para rangos y @Email para validar correo electrónico

--- QuieroProgramar-Spring-Boot/JAVA127-S79/SECCION_81/demo81-FORM2/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ QuieroProgramar-Spring-Boot/JAVA127-S79/SECCION_81/demo81-FORM2/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -1,6 +1,8 @@
 package com.bolsadeideas.springboot.form.app.models.domain;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -15,13 +17,15 @@ public class Usuario {
     @NotEmpty
     private String apellido;
 
-    @NotEmpty
+    @NotEmpty (message = "nuestro USUARIO no puede estar vasio (luego se mejorara en el properties)")
+    @Size(min = 3, max = 8)
     private String username;
 
     @NotEmpty
     private String password;
 
     @NotEmpty
+    @Email
     private String email;
 
 }
