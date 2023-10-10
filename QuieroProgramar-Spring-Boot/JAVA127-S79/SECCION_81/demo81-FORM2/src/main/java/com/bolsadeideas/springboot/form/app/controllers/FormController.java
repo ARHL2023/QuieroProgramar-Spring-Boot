033f456945a2c6b935c0ddc3e771895ac7b366f3
ARHL2023 <TuCorreoElectronico>
2023-10-10 09:28:53 -0500

    639 (El atributo object de thymeleaf en el elemento form)

--- QuieroProgramar-Spring-Boot/JAVA127-S79/SECCION_81/demo81-FORM2/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ QuieroProgramar-Spring-Boot/JAVA127-S79/SECCION_81/demo81-FORM2/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -14,32 +14,49 @@ import java.util.Map;
 @Controller
 public class FormController {
 
-	@GetMapping("/form")
-	public String form(Model model) {
-		Usuario usuario = new Usuario();
-		model.addAttribute("titulo", "Formulario usuarios");
-		model.addAttribute("usuario", usuario);
-		return "form";
-	}
+    @GetMapping("/form")
+    public String form(Model model) {
+        Usuario usuario = new Usuario();
+        model.addAttribute("titulo", "Formulario usuarios");
+        model.addAttribute("usuario", usuario);
+        return "form";
+    }
 
-	//Validación del formulario usando anotaciones
-	@PostMapping("/form")
-	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+    //Validación del formulario usando anotaciones
+//	@PostMapping("/form")
+//	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+//
+//		model.addAttribute("titulo", "Resultado form");
+//
+//		if(result.hasErrors()) {
+//			Map<String, String> errores = new HashMap<>();
+//			result.getFieldErrors().forEach(err ->{
+//				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+//			});
+//			model.addAttribute("error", errores);
+//			return "form";
+//		}
+//
+//		model.addAttribute("usuario", usuario);
+//
+//		return "resultado";
+//	}
 
-		model.addAttribute("titulo", "Resultado form");
 
-		if(result.hasErrors()) {
-			Map<String, String> errores = new HashMap<>();
-			result.getFieldErrors().forEach(err ->{
-				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
-			});
-			model.addAttribute("error", errores);
-			return "form";
-		}
+    //El atributo object de thymeleaf en el elemento form
+    @PostMapping("/form")
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
 
-		model.addAttribute("usuario", usuario);
+        model.addAttribute("titulo", "Resultado form");
 
-		return "resultado";
-	}
+        if (result.hasErrors()) {
+            return "form";
+        }
+
+        model.addAttribute("usuario", usuario);
+
+        return "resultado";
+    }
+
 
 }
